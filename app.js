fbbf8462748a04ddbc9dc3a35e4229e4319ce836
anonymoustafa <anonymoustafa@outlook.com>
2021-11-29 09:55:39 +0330

    Trying to use paper.js

---
+++ app.js
@@ -0,0 +1,1 @@
+const paper = require("paper");
--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('main.js loaded');
+$(document).ready(function () {
+	'use strict';
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+	//TODO
+	var c = Shape.Circle(200, 200, 55);
+  c.fillColor = 'green';
+	paper.view.draw();
+});
