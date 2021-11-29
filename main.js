d1d9854957f16276cf3c444f1d7b244eb6d0f65b
anonymoustafa <anonymoustafa@outlook.com>
2021-11-29 15:03:18 +0330

    Hello World! finally...

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+/// <reference path="./typings/globals/jquery/index.d.ts" />
+
 $(document).ready(function () {
 	'use strict';
 	paper.install(window);
-	paper.setup(document.getElementById('mainCanvas'));
-	//TODO
-	var c;
-  for (var x = 25; x < 400; x+=50) {
-		for (var y = 25; y < 400; y+=50) {
-			c = Shape.Circle(x,y,20);
-			c.fillColor = 'red';
-		}
-		
-	}
-
+	paper.setup(document.getElementById("mainCanvas"));
+	// var c = Shape.Circle(200,200,80);
+	// c.fillColor = 'yellow';
+	// var text = new PointText(200,200);
+	// text.justification = 'center';
+	// text.fillColor = 'black';
+	// text.fontSize = 15;
+	// text.content = 'Hello World!';
+	var tool = new Tool();
+	tool.onMouseDown = function(event) {
+		var c = Shape.Circle(event.point.x,event.point.y,20);
+		c.fillColor = 'red';
+	};
 	paper.view.draw();
 });
