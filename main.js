bef4beb4fd3de099960949d983435000e35213e0
anonymoustafa <anonymoustafa@outlook.com>
2021-11-29 15:05:13 +0330

    No, this one is hello world actually

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ $(document).ready(function () {
 	'use strict';
 	paper.install(window);
 	paper.setup(document.getElementById("mainCanvas"));
-	// var c = Shape.Circle(200,200,80);
-	// c.fillColor = 'yellow';
-	// var text = new PointText(200,200);
-	// text.justification = 'center';
-	// text.fillColor = 'black';
-	// text.fontSize = 15;
-	// text.content = 'Hello World!';
-	var tool = new Tool();
-	tool.onMouseDown = function(event) {
-		var c = Shape.Circle(event.point.x,event.point.y,20);
-		c.fillColor = 'red';
-	};
+	var c = Shape.Circle(200,200,80);
+	c.fillColor = 'yellow';
+	var text = new PointText(200,200);
+	text.justification = 'center';
+	text.fillColor = 'black';
+	text.fontSize = 15;
+	text.content = 'Hello World!';
+	// var tool = new Tool();
+	// tool.onMouseDown = function(event) {
+	// 	var c = Shape.Circle(event.point.x,event.point.y,20);
+	// 	c.fillColor = 'red';
+	// };
 	paper.view.draw();
 });
